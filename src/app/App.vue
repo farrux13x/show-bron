1707f32f37d3d1c17f6639ed57b0497767a6a1cf
farrux13x <abdusalomovf1208@gmail.com>
2026-02-10 23:00:04 +0500

    feat: add Telegram WebApp integration

--- src/app/App.vue
+++ src/app/App.vue
@@ -5,6 +5,19 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { RouterView } from 'vue-router';
 import AppShell from '@/widgets/app-shell/AppShell.vue';
+
+onMounted(() => {
+  if (window.Telegram?.WebApp) {
+    const tg = window.Telegram.WebApp;
+
+    tg.expand();
+
+    if (tg.requestFullscreen) {
+      tg.requestFullscreen();
+    }
+  }
+});
 </script>
